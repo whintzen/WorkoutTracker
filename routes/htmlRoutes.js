67a4716fcaa352c24e5cd4a67e121e42ff68d7ff
Wendy Hintzen <wpah@comcast.net>
2020-10-28 23:17:48 -0400

    pushing initial working files

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+var path = require("path");
+
+module.exports = function(app) {
+  //Get all the excercises 
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  //Get specific exercises
+  app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+  
+  //Get the stats
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
