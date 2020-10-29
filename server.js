a8fb55aa86f2abd240dff49546d4bb8df6ba7222
Wendy Hintzen <wpah@comcast.net>
2020-10-29 01:10:32 -0400

    Updated all the HTML files to point to the correct folder

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
 
 // db.Library.create({ name: "Campus Library" })
 //   .then(dbLibrary => {
